f1fa60bf71fe0647ea15e436034734013d2f5270
VictorLongberg <48484547+VictorLongberg@users.noreply.github.com>
2022-11-08 15:07:28 +0100

    testing google auth
    source: https://blog.logrocket.com/guide-adding-google-login-react-app/

--- react-bootstrap-app/src/App.js
+++ react-bootstrap-app/src/App.js
@@ -1,20 +1,64 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from "react";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
+
 function App() {
+    const [ profile, setProfile ] = useState([]);
+    const clientId = '1028880716536-iao1i40s8kl50b5mg1rdqetg9s1rqtgi.apps.googleusercontent.com';
+    useEffect(() => {
+        const initClient = () => {
+            gapi.client.init({
+                clientId: clientId,
+                scope: ''
+            });
+        };
+        gapi.load('client:auth2', initClient);
+    });
 
-  return(
+    const onSuccess = (res) => {
+        setProfile(res.profileObj);
+    };
 
-      <div className="App">
+    const onFailure = (err) => {
+        console.log('failed', err);
+    };
 
-     </div>
+    const logOut = () => {
+        setProfile(null);
+    };
 
-  )
-
+    return (
+        <div>
+            <h2>React Google Login</h2>
+            <br />
+            <br />
+            {profile ? (
+                <div>
+                    <img src={profile.imageUrl} alt="user image" />
+                    <h3>User Logged in</h3>
+                    <p>Name: {profile.name}</p>
+                    <p>Email Address: {profile.email}</p>
+                    <br />
+                    <br />
+                    <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
+                </div>
+            ) : (
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Sign in with Google"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+                />
+            )}
+        </div>
+    );
 }
-
 
 export default App;
