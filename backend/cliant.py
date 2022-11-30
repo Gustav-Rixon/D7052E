ba0b225406e8723073e50735d1c5ade1954f4eba
Gustav-Rixon <Gustav.rixon@gmail.com>
2022-11-30 21:52:39 +0100

    Implement cv2 server and client

---
+++ backend/cliant.py
@@ -0,0 +1,27 @@
+import socket,cv2, pickle,struct
+camera = True
+if camera == True:
+	vid = cv2.VideoCapture(0)
+else:
+    exit() #Should do som error handeling
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host_ip = '192.168.1.2'
+
+port = 8080
+client_socket.connect((host_ip,port))
+
+if client_socket: 
+	while (vid.isOpened()):
+		try:
+			img, frame = vid.read()
+			a = pickle.dumps(frame)
+			message = struct.pack("Q",len(a))+a
+			client_socket.sendall(message)
+			cv2.imshow(f"TO: {host_ip}",frame)
+			key = cv2.waitKey(1) & 0xFF
+			if key == ord("q"):
+				client_socket.close()
+		except:
+			print('VIDEO FINISHED!')
+			break
+
---
+++ backend/server.py
@@ -0,0 +1,53 @@
+#OBS THIS IS HARDCODES FOR 192.168.1.2
+#TODO I CANT CTRL C THE PROGRAM PLS HELP
+import socket, cv2, pickle, struct
+import threading
+import cv2
+
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host_name  = socket.gethostname()
+host_ip = '192.168.1.2'
+print('HOST IP:',"host_ip")
+port = 8080
+socket_address = (host_ip,port)
+server_socket.bind(socket_address)
+server_socket.listen()
+print("Listening at",socket_address)
+
+def show_client(addr,client_socket):
+	try:
+		print('CLIENT {} CONNECTED!'.format(addr))
+		if client_socket: # if a client socket exists
+			data = b""
+			payload_size = struct.calcsize("Q")
+			while True:
+				while len(data) < payload_size:
+					packet = client_socket.recv(4*1024) # 4K
+					if not packet: break
+					data+=packet
+				packed_msg_size = data[:payload_size]
+				data = data[payload_size:]
+				msg_size = struct.unpack("Q",packed_msg_size)[0]
+				
+				while len(data) < msg_size:
+					data += client_socket.recv(4*1024)
+				frame_data = data[:msg_size]
+				data  = data[msg_size:]
+				frame = pickle.loads(frame_data)
+				cv2.imshow(f"FROM {addr}",frame)
+				key = cv2.waitKey(1) & 0xFF
+				if key  == ord('q'):
+					break
+			client_socket.close()
+	except Exception as e:
+		print(f"CLINET {addr} DISCONNECTED")
+		pass
+		
+while True:
+	client_socket,addr = server_socket.accept()
+	thread = threading.Thread(target=show_client, args=(addr,client_socket))
+	thread.start()
+	print("TOTAL CLIENTS ",threading.active_count() - 1)
+	
+				
+
