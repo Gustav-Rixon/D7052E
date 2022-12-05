0334ecca395052d0aefc696d5da7bb970ee01d7e
edisonw98 <73946718+edisonw98@users.noreply.github.com>
2022-12-05 19:41:06 +0100

    now adds the joiner to the json

--- Hub/newpi.py
+++ Hub/newpi.py
@@ -2,48 +2,48 @@
 import json
 from pathlib import Path
 
-with open("..//Hub/Storage/cameras.json") as f:
-  data = json.load(f)
-  
-idip=[]
-for p_id in data:
-    idip.append((p_id['id'], p_id['ip']))
-    
-print( all("92.35.224.111" == a[1] for a in idip))
-print(idip)
-print((idip[-1][0] + 1))
 #structure example
 class Newpi:
     
-    #adds the ip to the json calling on help methods to do we want it to 
-    def join(ip):
-        with open("..//Hub/Storage/cameras.json") as f:
-            data = json.load(f)
-        temp = idiplist(data)
-        if idlist(data):
-            
-            return "already exists"
-        else:
-            id = assignid()
-            generatefolder(id)
-            return "Join succ"
-    
     #generates the list of all ids
-    def idiplist(data):
+    def idiplist(self, data):
        idip=[]
        for p_id in data:
-        ids.append((p_id['id'], p_id['ip']))
+        idip.append((p_id['id'], p_id['ip']))
        return idip
     
     #Generates a folder in  if one doesnt exist to store all the 
-    def generatefolder(id):
+    def generatefolder(self, id):
         Path("..//Hub/Storage/Cameras/" + str(id) ).mkdir(parents=True, exist_ok=True)
     
     #tror inte jag behöver metod för detta bestämmer mig senare (lägger till 1 i id från sista numret i listan av cameror)
-    def assignid(list):
+    def assignid(self, list):
         return  (list[-1][0] + 1)
     
     #tror inte jag behöver metod för detta bestämmer mig senare (kollar om ipt finns sen innan så det inte sker dupes )
-    def ipexists(data, ip):
-        return all("92.35.224.111" == a[1] for a in idip)
+    def ipexists(self, list, ip):
+        return all("92.35.224.111" == a[1] for a in list)
+    
+        #adds the ip to the json calling on help methods to do we want it to 
+    def joinnet(self, ip):
+        with open("..//Hub/Storage/cameras.json") as f:
+            data = json.load(f)
+        temp = self.idiplist(data)
+        if self.ipexists(temp, ip):
+            
+            return "already exists"
+        else:
+            id = self.assignid(temp)
+            self.generatefolder(id)
+            # Data to be written
+            data.append({
+                "id": id,
+                "ip": ip,
+                "name": ""
+            }) 
+            with open("..//Hub/Storage/cameras.json", "w") as outfile:
+                json.dump(data, outfile, 
+                        indent=4,  
+                        separators=(',',': '))
+            return "Join succ"
     
---
+++ Hub/sim.py
@@ -0,0 +1,5 @@
+#TODO some kind of heartbeat? and check download attachments from the mail and add them to the hub storage every x time?
+
+import newpi
+t = newpi.Newpi()
+t.joinnet("1.1.1.1")
