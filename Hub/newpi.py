905ea76acc3495f5dad026744bcce547ea9ee05b
edisonw98 <73946718+edisonw98@users.noreply.github.com>
2022-12-08 10:59:17 +0100

    bug fix

--- Hub/newpi.py
+++ Hub/newpi.py
@@ -22,16 +22,21 @@ class Newpi:
     
     #tror inte jag behöver metod för detta bestämmer mig senare (kollar om ipt finns sen innan så det inte sker dupes )
     def ipexists(self, list, ip):
-        return all("92.35.224.111" == a[1] for a in list)
+        for a in list:
+            for b in a:
+                if b == ip:
+                    return True
+        return False
     
         #adds the ip to the json calling on help methods to do we want it to 
     def joinnet(self, ip):
         with open("..//Hub/Storage/cameras.json") as f:
             data = json.load(f)
         temp = self.idiplist(data)
+        print(self.ipexists(temp, ip))
         if self.ipexists(temp, ip):
-            
-            return "already exists"
+            return print("already exists")
+        #TODO maybe fix a json template as elif
         else:
             id = self.assignid(temp)
             self.generatefolder(id)
@@ -47,3 +52,4 @@ class Newpi:
                         separators=(',',': '))
             return "Join succ"
     
+#TODO behöver en endpoint som connectar detta med kameror sen behöver jag fixa camerakoden som connectar till detta.
