4964ed7ed272c0fc9a60bc6886f8cadeea417e11
edisonw98 <73946718+edisonw98@users.noreply.github.com>
2022-12-29 15:10:40 +0100

    fix stuff + actually uploads

--- Pi/main.py
+++ Pi/main.py
@@ -3,7 +3,8 @@ import camera
 import mail
 import threading
 import sensor
-
+import upload
+import join
 
 class Main:
 
@@ -12,21 +13,33 @@ class Main:
         self.sensor_main = sensor.Sensor()
         # Camera client
         self.camera_main = camera.Camera()
-        
         # Email client
         self.mail_main = mail.Email()
+        #upload to drive client
+        self.upload_main = upload.Upload()
     
     def start(self):
         print("starting...")
         while True:
+            record_camera_thread = threading.Thread(target=self.camera_main.record, args=())
+            mail_thread = threading.Thread(target=self.mail_main.alert, args=())
+            upload_thread = threading.Thread(target=self.mail_main.alert, args=())
             if(self.sensor_main.getvalue() == 0):
                 self.sensor_main.stale()
             elif(self.sensor_main.getvalue() == 1):
-                self.camera_main.record()
-                mail_thread = threading.Thread(target=self.mail_main.alert, args=())
+                #self.camera_main.record()
+                print(f"recording...")
+                record_camera_thread.start()
+                record_camera_thread.join()
+                print(f'sending email...')
+                mail_thread.start()
+                print(f"uploading")
+                upload_thread.start()
                 # Check status if home or not
-                is_home = self.mail_main.check()
+                mail_thread.join()
+                upload_thread.join()
                 self.sensor_main.motion()
+                #camera_main.archive()
                 
 """""
             # Printing that the system is on and checking
--- Pi/upload.py
+++ Pi/upload.py
@@ -7,13 +7,22 @@ from dotenv import load_dotenv
 load_dotenv()
 DRIVE_ID = os.getenv('DRIVE_ID')
 driveID = DRIVE_ID
+FILE_PATH = os.getenv('FILE_PATH')
+imagePath = FILE_PATH
 class Upload:
     
-    def uploader(self, movie, pic):
+    def uploader(self):
         gauth = GoogleAuth()           
-        drive = GoogleDrive(gauth)
-
-        upload_file_list = [movie]
+        drive = GoogleDrive(gauth)  
+             
+        for filename in os.listdir(imagePath):
+            new_path = f"{imagePath}{filename}"
+            with open(new_path, 'rb') as file:
+                file_data = file.read()
+                file_name = file.name
+                print(f"filnamnet fan {file.name}")
+        
+        upload_file_list = [file_data]
         for upload_file in upload_file_list:
             gfile = drive.CreateFile({'parents': [{'id': driveID}]})
             # Read file and set it as the content of this instance.
