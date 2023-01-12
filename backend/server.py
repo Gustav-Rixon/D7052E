6b3abcb8a777a0b04ad9d7a4e5c8584f9f3523cc
VictorLongberg <48484547+VictorLongberg@users.noreply.github.com>
2023-01-12 13:44:02 +0100

    asd

--- backend/server.py
+++ backend/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host_name  = socket.gethostname()
-host_ip = '192.168.1.125'
+host_ip = '130.240.155.243'
 print('HOST IP:',"host_ip")
 port = 8080
 socket_address = (host_ip,port)
