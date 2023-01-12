7a338ba61d951fdacb681d96fdbab8673fc4cebb
Gustav-Rixon <Gustav.rixon@gmail.com>
2023-01-12 21:18:59 +0100

    Update test2.py

--- backend/test2.py
+++ backend/test2.py
@@ -1,4 +1,6 @@
 import requests
 
-response = requests.post("http://localhost:8080/", data="argument=value")
+response2 = requests.post("http://127.0.0.1:1337/video_feed_terminate/0/")
+response = requests.post("http://localhost:8080/")
 print(response.text)
+#print(response2.text)
